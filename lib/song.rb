302bb4b99b192b01992cb88bdc3befd4201f5317
MarlonMo37 <marlonmoreno894@gmail.com>
2020-11-07 18:24:13 -0600

    commit tree 78c53920a572845ac06c85e97ec2b597c8d4b43e

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,67 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name 
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find {|s| s.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|a| a.name}
+  end
+
+  def self.new_from_filename(file)
+    split  = file.split(" - ")
+    artist_name = split[0]
+    song_name_file = split[1]
+    split_two = song_name_file.split(".")
+    song_name = split_two[0]
+    
+    
+    
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name 
+    song
+  end
+
+    def self.create_from_filename(file)
+      split  = file.split(" - ")
+      artist_name = split[0]
+      song_name_file = split[1]
+      split_two = song_name_file.split(".")
+      song_name = split_two[0]
+
+      song = self.create
+      song.name = song_name
+      song.artist_name = artist_name
+      song
+      
+    end
+
+    def self.destroy_all
+      self.all.clear
+    end
+
 end
